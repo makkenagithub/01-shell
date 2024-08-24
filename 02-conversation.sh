57629b60c3178ceb1aa08555977824a9b8ebc259
Suresh Makkena <suresh.makkena@gmail.com>
2024-08-24 17:44:49 +0530

    varaibles concept

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: hi, how are you"
+echo "$PERSON2: i am fine, how about you"
+echo "$PERSON1: i am also good"
---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "enter user name:"
+read USERNAME
+echo "user name is $USERNAME"
+echo "enter password:"
+read -s PASSWORD
+echo "passowrd is $PASSWORD"
+
+
