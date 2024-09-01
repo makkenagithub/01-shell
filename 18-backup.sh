c0655f428f9c8b6cf9abe1e267398938ee6eca8b
makkenagithub <suresh.makkena@gmail.com>
2024-09-01 15:54:40 +0530

    backup of files

--- 18-backup.sh
+++ 18-backup.sh
@@ -31,7 +31,7 @@ fi
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
-if [[ ! -z $FILES ]]  # if $FILES is not empty. single [ gave error initially. [[ worked fine 
+if [[ ! -z $FILES ]]  # if $FILES is not empty. single [ gave error initially. [[ worked fine
 then
     echo "Files older than $DAYS exist"
     ZIP_FILE="$DEST_DIR/app-logs-$TIME_STAMP.zip"
