ad2d2740494892f9ada2072b626567e5b6841654
makkenagithub <suresh.makkena@gmail.com>
2024-09-01 15:10:39 +0530

    file system alerts

--- 19-diskusage.sh
+++ 19-diskusage.sh
@@ -8,7 +8,7 @@ while IFS= read -r LINES
 do
     CURRENT_USAGE=$(echo $LINES | awk -F " " '{print $6F}' | cut -d "%" -f1)    #6F gets 6th fragment and cut % filed
     PARTITION=$(echo $LINES | awk -F " " '{print $NF}') #NF is to get last fragment
-    if [ CURRENT_USAGE -ge THRESHOLD ]
+    if [ $CURRENT_USAGE -ge $THRESHOLD ]
     then
         echo "Filesystem $PARTITION is $CURRENT_USAGE% full. Please check."
     fi
